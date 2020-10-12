012b2cdec8772828c4b08436c63092d3e7921b95
lxhao61 <lxhao61@gmail.com>
2020-10-12 21:15:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 注释掉lienol大diy1源
-#sed -i 's/^\(.*diy1\)/#&/' feeds.conf.default
+sed -i 's/^\(.*diy1\)/#&/' feeds.conf.default
 
 # 添加修改后的diy1源
-#sed -i '$a src-git diy1 https://github.com/lxhao61/openwrt-package.git;master' feeds.conf.default
+sed -i '$a src-git diy1 https://github.com/lxhao61/openwrt-package.git;master' feeds.conf.default
